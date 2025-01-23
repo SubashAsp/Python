3bb4734a1ee32e6a764dfee1acb01831b3ccd6f0
SubashAsp <subasht0731@gmail.com>
2025-01-23 19:26:47 +0530

    set

---
+++ set.py
@@ -0,0 +1,21 @@
+print("Set")
+
+#creating a set 
+a = {1, 2, 3, 1}
+print(a)  #does not allows duplicate
+
+b = {"apple", "banana", "cherry", True, 1, 2, 0, False}
+print(b)
+
+#lenght
+print(len(b))
+
+#checking membership
+print('apple' in b)
+print('subash' in b)
+
+#set operatipns
+c = set('abracadabra')
+d = set('alacazam')
+print(c)
+print(d)
