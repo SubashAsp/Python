5897fafef562744cff7f988239e5d98532962573
SubashAsp <subasht0731@gmail.com>
2025-01-28 17:13:18 +0530

    add() method

--- set.py
+++ set.py
@@ -27,3 +27,37 @@ print(c ^ d)  #NOR--in a or b but not both
 #comperhension in set 
 e = {x for x in 'abracadabra' if x not in 'abc'}
 print(e)
+
+#set methods
+
+# add() -- addinfg single element to a set
+add1 = {1, 2, 3, 4}
+add1.add('subash')
+print("Adding elements to set : ", add1)
+
+# Only one element can be added using add method, it gives error to two elements at same time
+# add2 = {1, 2, 3, 4}
+# add2.add(10, "subash")
+# print("Adding two elements to a set : ", add2)
+
+#add one after another
+add3 = {1, 2, 3, 4}
+add3.add(10)
+add3.add("subash")
+print("Adding two elements", add3)
+
+add_set = {1, 2, 3, 4, 5}
+add_set.add(6)
+print("Adding element to a set : ",add_set)
+
+# only immutabe objects can be added to a set
+# add_set1 = {1, 2, 3, 4}
+# add_set2 = {5}
+# add_set1.add(add_set2)
+# print(add_set1)
+
+add_set3 = {1, 2, 3, 4}
+add_set4 = (5)
+add_set3.add(add_set4)
+print("Adding immutable object to a set : ", add_set3)
+
