3f38b34cb2176bc94514f96e82a63410addc127c
SubashAsp <subasht0731@gmail.com>
2025-01-29 12:35:13 +0530

    name space, method, local scope

--- 29-01/class.py
+++ 29-01/class.py
@@ -2,8 +2,8 @@ print("")
 #class
 
 class new_class:
-
-    def state(self):
+    # namespace is created
+    def state(self):  # method (function)
         print("Hello.")
 
 class second_class:
@@ -17,3 +17,15 @@ new_class.state(greet)  #  method to call function  inside a class
 greet.state()  # calling using a class object 
 
 # greet2.state()  // you cannot call a method in a class with different class object
+
+# declaring a local scope
+class new_cls:
+    def new_fun():
+        x = 10  # local scope / local variable
+
+# () is used to call the class or create an instance
+num = new_cls()  
+# instance is created
+
+# print(x)
+# we cannot call a variable that iss defined inside a method outside directly
