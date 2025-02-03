e062960316b757a5903a81df5076681eb8afca39
SubashAsp <subasht0731@gmail.com>
2025-02-03 14:05:26 +0530

    inheritance

---
+++ 03-02/inheritance.py
@@ -0,0 +1,58 @@
+print("Inheritance")
+
+# inheritance -- inherit from single parent class
+class Teacher:
+    def greet(self):
+        return f"Good morning everyone."
+    
+class Student(Teacher):
+    pass
+    
+student = Student()
+print(student.greet())
+
+print("Multiple Inheritance : ")
+
+# multiple inheritance -- inherit from multiple parent class
+class Dad:
+    def dad_wisdom(self):
+        return "Dad's wisdom."
+    
+class Mom:
+    def mom_wisdom(self):
+        return "Mom's wisdom."
+    
+class Child(Dad, Mom):
+    pass
+
+child = Child()
+print(child.dad_wisdom())
+print(child.mom_wisdom())
+
+print("Multilevel Inheritance : ")
+
+# multilevel inheritance -- inherit from parent class and turn as parent class for another class
+
+# hierarchical inheritance
+class College:
+    def __init__(self, name):
+        self.name = name
+
+    def department(self):
+        return f"{self.name} is from NEC"
+    
+class Info_Tech(College):
+    def department(self):
+        return f"{self.name} is from Information Technology."
+    
+class Ece(College):
+    def department(self):
+        return f"{self.name} is from Electronics and Communication Engineering."
+    
+        
+info_tech = Info_Tech("Subash")
+ece = Ece("Logu")
+clg = College("Karthi")
+print(info_tech.department())
+print(ece.department())
+print(clg.department())
