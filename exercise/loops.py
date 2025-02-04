10dd2ee7a66713335f82556bd622b27fa874a039
SubashAsp <subasht0731@gmail.com>
2025-02-04 18:27:30 +0530

    update

--- exercise/loops.py
+++ exercise/loops.py
@@ -12,9 +12,9 @@ print(total)
 
 # 2.Write a Python program that takes a list of integers and counts how many even and odd numbers are present using a for loop.
 
-# numbers = [1, 1, 3, 3, 5, 6, 7, 8, 9]
-numbers = input("Enter a list for checking odd or even numbers.")
-numbers = numbers.split()
+numbers = [1, 1, 3, 3, 5, 6, 7, 8, 9]
+# numbers = input("Enter a list for checking odd or even numbers.")
+# numbers = numbers.split()
 odd_count = 0
 even_count = 0
 for i in numbers:
@@ -26,3 +26,21 @@ for i in numbers:
 
 print("Even numbers in given list :", even_count)
 print("Odd numbers in given list :", odd_count)
+
+# nested for loop
+
+# 1.Write a Python program using nested for loops to print the multiplication table from 1 to 10.
+
+for i in range(10):
+    print(f"{i} Table")
+    for j in range(10):
+        k = i * j
+        print(f"{j} x {i} = {k}")
+
+# 2.Write a Python program to print a right-angled triangle made of stars (*), where the number of rows is given by the user.
+
+row = int(input("Enter the number rows wanted : "))
+for i in range(row + 1):
+    for j in range(i):
+        print("* ", end = "")
+    print()
