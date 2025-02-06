5573f80899eb41ef56754cced9d4d5a4804f70f8
SubashAsp <subasht0731@gmail.com>
2025-02-06 14:47:33 +0530

    lambda functon

---
+++ 06-02/lambda.py
@@ -0,0 +1,15 @@
+# adding numbers using lambda functions
+add = lambda x,y:x+y
+print(add(23,7))
+
+sub = lambda x,y:x-y
+print(sub(23,3))
+
+multi = lambda x,y:x*y
+print(multi(5, 6))
+
+div = lambda x,y:x/y
+print(div(200, 10))
+
+div1 = lambda x,y:x//y
+print(div1(200, 10))
