aa7044444d6360b8ecbd0970d066077398f3a3ab
SubashAsp <subasht0731@gmail.com>
2025-02-07 15:00:36 +0530

    try & except

--- 04-02/error_Handling.py
+++ 04-02/error_Handling.py
@@ -1,7 +1,19 @@
-while True:
-    try:
-        x = int(input("Enter a number : "))
-        break
-    except ValueError:
-        print("enter the correct value")
+# while True:
+#     try:
+#         x = int(input("Enter a number : "))
+#         break
+#     except ValueError:
+#         print("enter the correct value")
         
+# # user given value
+# while True:
+#     try:
+#         user_given_number = int(input("Enter a number for dividing : "))
+#         print(1/user_given_number)
+#         break
+#     except ZeroDivisionError:
+#         print("Cannot enter zero for divisior.")
+#     except ValueError:
+#         print("Please enter a number.")
+#     except Exception:
+#         print("Something went wrong.")
